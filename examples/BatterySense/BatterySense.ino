e753f769f719719efa3166d49d1ea57d1c022634
Justin <JayPalm@users.noreply.github.com>
2022-05-07 18:46:02 -0700

    Update example code
    Fix typo in example

--- examples/BatterySense/BatterySense.ino
+++ examples/BatterySense/BatterySense.ino
@@ -17,7 +17,7 @@ void loop() {
   unsigned long m = millis();
   if(m - secondsTimer > 1000L){
     secondsTimer = m;
-    mppt.ping();  // send oing every second
+    mppt.ping();  // send ping every second
   }
 }
 
